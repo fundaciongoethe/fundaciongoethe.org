7507aefaffaef568821c625ac5d839fb5c1545ac
madrilene <hola@lenesaile.com>
2021-05-19 19:43:53 +0200

    nav logik und footer

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,62 +1,203 @@
 module.exports = {
-	es: [
-		{
-			text: "Inico",
-			url: "/es/",
+	es: {
+		top: [
+			{
+				text: "Inico",
+				url: "/es/",
+			},
+			{
+				text: "La Fundación",
+				url: "/es/sobre-nosotros/",
+			},
+			{
+				text: "Eventos",
+				url: "/es/eventos/",
+			},
+			{
+				text: "Artistas",
+				url: "/es/artistas/",
+			},
+			{
+				text: "Lugares",
+				url: "/es/lugares/",
+			},
+			{
+				text: "Noticias",
+				url: "/es/noticias/",
+			},
+			{
+				text: "Contacto",
+				url: "/es/contacto/",
+			},
+		],
+
+		bottom: {
+			row1: [
+				{
+					text: "La Fundación",
+					url: "/es/sobre-nosotros/",
+				},
+				{
+					text: "Patronato",
+					url: "/es/sobre-nosotros#patronato/",
+				},
+				{
+					text: "Patrocinadores",
+					url: "/es/sobre-nosotros#patrocinadores/",
+				},
+				{
+					text: "Partners",
+					url: "/es/sobre-nosotros#partners/",
+				},
+			],
+			row2: [
+				{
+					text: "Eventos",
+					url: "/es/eventos/",
+				},
+				{
+					text: "Artistas",
+					url: "/es/artistas/",
+				},
+				{
+					text: "Orte",
+					url: "/es/lugares/",
+				},
+				{
+					text: "Albúm de fotos",
+					url: "/es/",
+				},
+			],
+			row3: [
+				{
+					text: "Invitaciones",
+					url: "/es/",
+				},
+				{
+					text: "Förderer werden",
+					url: "/es/",
+				},
+				{
+					text: "Noticias",
+					url: "/es/noticias/",
+				},
+				{
+					text: "Contacto",
+					url: "/es/contacto",
+				},
+			],
 		},
-		{
-			text: "La Fundación",
-			url: "/es/sobre-nosotros/",
+
+		footer: [
+			{
+				text: "Aviso legal",
+				url: "/es/aviso/",
+			},
+			{
+				text: "Política de privacidad ",
+				url: "/es/privacidad/",
+			},
+		],
+	},
+
+	de: {
+		top: [
+			{
+				text: "Home",
+				url: "/de/",
+			},
+			{
+				text: "Die Fundación",
+				url: "/de/ueber-uns/",
+			},
+			{
+				text: "Events",
+				url: "/de/events/",
+			},
+			{
+				text: "Künstler",
+				url: "/de/kuenstler/",
+			},
+			{
+				text: "Orte",
+				url: "/de/orte/",
+			},
+			{
+				text: "Aktuelles",
+				url: "/de/aktuelles/",
+			},
+			{
+				text: "Kontakt",
+				url: "/de/kontakt/",
+			},
+		],
+
+		bottom: {
+			row1: [
+				{
+					text: "Die Fundación",
+					url: "/de/ueber-uns/",
+				},
+				{
+					text: "Vorstand",
+					url: "/de/ueber-uns#patronato/",
+				},
+				{
+					text: "Förderer",
+					url: "/de/ueber-uns#patrocinadores/",
+				},
+				{
+					text: "Kooperationspartner",
+					url: "/de/ueber-uns#partners/",
+				},
+			],
+			row2: [
+				{
+					text: "Events",
+					url: "/de/events/",
+				},
+				{
+					text: "Künstler",
+					url: "/de/kuenstler/",
+				},
+				{
+					text: "Orte",
+					url: "/de/orte/",
+				},
+				{
+					text: "Fotoalbum",
+					url: "/de/",
+				},
+			],
+			row3: [
+				{
+					text: "Einladungen",
+					url: "/de/",
+				},
+				{
+					text: "Förderer werden",
+					url: "/de/",
+				},
+				{
+					text: "Aktuelles",
+					url: "/de/aktuelles/",
+				},
+				{
+					text: "Kontakt",
+					url: "/de/kontakt",
+				},
+			],
 		},
-		{
-			text: "Eventos",
-			url: "/es/eventos/",
-		},
-		{
-			text: "Artistas",
-			url: "/es/artistas/",
-		},
-		{
-			text: "Lugares",
-			url: "/es/lugares/",
-		},
-		{
-			text: "Noticias",
-			url: "/es/noticias/",
-		},
-		{
-			text: "Contacto",
-			url: "/es/contacto/",
-		},
-	],
-	de: [
-		{
-			text: "Home",
-			url: "/de/",
-		},
-		{
-			text: "Die Fundación",
-			url: "/de/ueber-uns/",
-		},
-		{
-			text: "Events",
-			url: "/de/events/",
-		},
-		{
-			text: "Künstler",
-			url: "/de/kuenstler/",
-		},
-		{
-			text: "Orte",
-			url: "/de/orte/",
-		},
-		{
-			text: "Aktuelles",
-			url: "/de/aktuelles/",
-		},
-		{
-			text: "Kontakt",
-			url: "/de/kontakt/",
-		},
-	],
+
+		footer: [
+			{
+				text: "Impressum",
+				url: "/de/impressum/",
+			},
+			{
+				text: "Datenschutzerklärung ",
+				url: "/de/datenschutz/",
+			},
+		],
+	},
 };
