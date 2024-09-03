f676047544e7604696ef51f31b2b177d01d908a1
madrilene <hola@lenesaile.com>
2024-09-03 13:54:27 +0200

    make logo reachable via keyboard, remove redundant home link

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,225 +1,217 @@
 module.exports = {
-	es: {
-		top: [
-			{
-				text: "Inico",
-				url: "/es/",
-			},
-			{
-				text: "La Fundación",
-				url: "/es/sobre-nosotros/",
-			},
-			{
-				text: "Eventos",
-				url: "/es/eventos/",
-			},
-			{
-				text: "Artistas",
-				url: "/es/artistas/",
-			},
-			{
-				text: "Lugares",
-				url: "/es/lugares/",
-			},
-			{
-				text: "Noticias",
-				url: "/es/noticias/",
-			},
-			{
-				text: "Contacto",
-				url: "/es/contacto/",
-			},
-		],
+  es: {
+    top: [
+      {
+        text: 'La Fundación',
+        url: '/es/sobre-nosotros/',
+      },
+      {
+        text: 'Eventos',
+        url: '/es/eventos/',
+      },
+      {
+        text: 'Artistas',
+        url: '/es/artistas/',
+      },
+      {
+        text: 'Lugares',
+        url: '/es/lugares/',
+      },
+      {
+        text: 'Noticias',
+        url: '/es/noticias/',
+      },
+      {
+        text: 'Contacto',
+        url: '/es/contacto/',
+      },
+    ],
 
-		bottom: {
-			row1: [
-				{
-					text: "La Fundación",
-					url: "/es/sobre-nosotros/",
-				},
-				{
-					text: "Patronato",
-					url: "/es/sobre-nosotros/#patronato",
-				},
-				{
-					text: "Promotores",
-					url: "/es/sobre-nosotros/#sponsors",
-				},
-				{
-					text: "Partners",
-					url: "/es/sobre-nosotros/#partners",
-				},
-			],
-			row2: [
-				{
-					text: "Eventos",
-					url: "/es/eventos/",
-				},
-				{
-					text: "Artistas",
-					url: "/es/artistas/",
-				},
-				{
-					text: "Lugares",
-					url: "/es/lugares/",
-				},
-				{
-					text: "Álbum de fotos",
-					url: "/es/galeria/",
-				},
-			],
-			row3: [
-				{
-					text: "Invitaciones",
-					url: "/es/invitaciones/",
-				},
-				{
-					text: "Participar",
-					url: "/es/participar/",
-				},
-				{
-					text: "Noticias",
-					url: "/es/noticias/",
-				},
-				{
-					text: "Contacto",
-					url: "/es/contacto/",
-				},
-			],
-		},
+    bottom: {
+      row1: [
+        {
+          text: 'La Fundación',
+          url: '/es/sobre-nosotros/',
+        },
+        {
+          text: 'Patronato',
+          url: '/es/sobre-nosotros/#patronato',
+        },
+        {
+          text: 'Promotores',
+          url: '/es/sobre-nosotros/#sponsors',
+        },
+        {
+          text: 'Partners',
+          url: '/es/sobre-nosotros/#partners',
+        },
+      ],
+      row2: [
+        {
+          text: 'Eventos',
+          url: '/es/eventos/',
+        },
+        {
+          text: 'Artistas',
+          url: '/es/artistas/',
+        },
+        {
+          text: 'Lugares',
+          url: '/es/lugares/',
+        },
+        {
+          text: 'Álbum de fotos',
+          url: '/es/galeria/',
+        },
+      ],
+      row3: [
+        {
+          text: 'Invitaciones',
+          url: '/es/invitaciones/',
+        },
+        {
+          text: 'Participar',
+          url: '/es/participar/',
+        },
+        {
+          text: 'Noticias',
+          url: '/es/noticias/',
+        },
+        {
+          text: 'Contacto',
+          url: '/es/contacto/',
+        },
+      ],
+    },
 
-		footer: [
-			{
-				text: "Aviso legal",
-				url: "/es/aviso-legal/",
-			},
-			{
-				text: "Política de privacidad ",
-				url: "/es/privacidad/",
-			},
-		],
+    footer: [
+      {
+        text: 'Aviso legal',
+        url: '/es/aviso-legal/',
+      },
+      {
+        text: 'Política de privacidad ',
+        url: '/es/privacidad/',
+      },
+    ],
 
-		credit: [
-			{
-				text: "Lene Saile",
-				url: "https://www.lenesaile.com",
-			},
-			{
-				text: "Sobre esta página",
-				url: "/es/sobre-esta-pagina/",
-			},
-		],
-	},
+    credit: [
+      {
+        text: 'Lene Saile',
+        url: 'https://www.lenesaile.com',
+      },
+      {
+        text: 'Sobre esta página',
+        url: '/es/sobre-esta-pagina/',
+      },
+    ],
+  },
 
-	de: {
-		top: [
-			{
-				text: "Home",
-				url: "/de/",
-			},
-			{
-				text: "Die Fundación",
-				url: "/de/ueber-uns/",
-			},
-			{
-				text: "Events",
-				url: "/de/events/",
-			},
-			{
-				text: "Künstler",
-				url: "/de/kuenstler/",
-			},
-			{
-				text: "Orte",
-				url: "/de/orte/",
-			},
-			{
-				text: "Aktuelles",
-				url: "/de/aktuelles/",
-			},
-			{
-				text: "Kontakt",
-				url: "/de/kontakt/",
-			},
-		],
+  de: {
+    top: [
+      {
+        text: 'Die Fundación',
+        url: '/de/ueber-uns/',
+      },
+      {
+        text: 'Events',
+        url: '/de/events/',
+      },
+      {
+        text: 'Künstler',
+        url: '/de/kuenstler/',
+      },
+      {
+        text: 'Orte',
+        url: '/de/orte/',
+      },
+      {
+        text: 'Aktuelles',
+        url: '/de/aktuelles/',
+      },
+      {
+        text: 'Kontakt',
+        url: '/de/kontakt/',
+      },
+    ],
 
-		bottom: {
-			row1: [
-				{
-					text: "Die Fundación",
-					url: "/de/ueber-uns/",
-				},
-				{
-					text: "Vorstand",
-					url: "/de/ueber-uns/#patronato",
-				},
-				{
-					text: "Förderer",
-					url: "/de/ueber-uns/#sponsors",
-				},
-				{
-					text: "Partner",
-					url: "/de/ueber-uns/#partners",
-				},
-			],
-			row2: [
-				{
-					text: "Events",
-					url: "/de/events/",
-				},
-				{
-					text: "Künstler",
-					url: "/de/kuenstler/",
-				},
-				{
-					text: "Orte",
-					url: "/de/orte/",
-				},
-				{
-					text: "Fotoalbum",
-					url: "/de/fotoalbum/",
-				},
-			],
-			row3: [
-				{
-					text: "Einladungen",
-					url: "/de/einladungen/",
-				},
-				{
-					text: "Mitmachen",
-					url: "/de/mitmachen/",
-				},
-				{
-					text: "Aktuelles",
-					url: "/de/aktuelles/",
-				},
-				{
-					text: "Kontakt",
-					url: "/de/kontakt/",
-				},
-			],
-		},
+    bottom: {
+      row1: [
+        {
+          text: 'Die Fundación',
+          url: '/de/ueber-uns/',
+        },
+        {
+          text: 'Vorstand',
+          url: '/de/ueber-uns/#patronato',
+        },
+        {
+          text: 'Förderer',
+          url: '/de/ueber-uns/#sponsors',
+        },
+        {
+          text: 'Partner',
+          url: '/de/ueber-uns/#partners',
+        },
+      ],
+      row2: [
+        {
+          text: 'Events',
+          url: '/de/events/',
+        },
+        {
+          text: 'Künstler',
+          url: '/de/kuenstler/',
+        },
+        {
+          text: 'Orte',
+          url: '/de/orte/',
+        },
+        {
+          text: 'Fotoalbum',
+          url: '/de/fotoalbum/',
+        },
+      ],
+      row3: [
+        {
+          text: 'Einladungen',
+          url: '/de/einladungen/',
+        },
+        {
+          text: 'Mitmachen',
+          url: '/de/mitmachen/',
+        },
+        {
+          text: 'Aktuelles',
+          url: '/de/aktuelles/',
+        },
+        {
+          text: 'Kontakt',
+          url: '/de/kontakt/',
+        },
+      ],
+    },
 
-		footer: [
-			{
-				text: "Impressum",
-				url: "/de/impressum/",
-			},
-			{
-				text: "Datenschutz",
-				url: "/de/datenschutz/",
-			},
-		],
+    footer: [
+      {
+        text: 'Impressum',
+        url: '/de/impressum/',
+      },
+      {
+        text: 'Datenschutz',
+        url: '/de/datenschutz/',
+      },
+    ],
 
-		credit: [
-			{
-				text: "Lene Saile",
-				url: "https://www.lenesaile.com/de",
-			},
-			{
-				text: "Über diese Seite",
-				url: "/de/ueber-diese-seite/",
-			},
-		],
-	},
+    credit: [
+      {
+        text: 'Lene Saile',
+        url: 'https://www.lenesaile.com/de',
+      },
+      {
+        text: 'Über diese Seite',
+        url: '/de/ueber-diese-seite/',
+      },
+    ],
+  },
 };
